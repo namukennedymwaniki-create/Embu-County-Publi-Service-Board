e4b1f2b7d9b3cecc58e015ab80877a048d12e127
namukennedymwaniki-create <namukennedymwaniki@gmail.com>
2026-05-26 22:46:26 +0300

    Update streamlit_app_update.py

--- streamlit_app_update.py
+++ streamlit_app_update.py
@@ -2001,34 +2001,35 @@ def hr_dashboard():
                                 index=["Administration", "Finance", "Human Resource", "ICT", "Health", "Education", "Public Works", "Agriculture", "Other"].index(emp['department']) if emp['department'] in ["Administration", "Finance", "Human Resource", "ICT", "Health", "Education", "Public Works", "Agriculture", "Other"] else 0,
                                 key="edit_department")
                             
-                            # Handle first appointment date - NO DATE LIMIT
+                            # Handle first appointment date - WIDE DATE RANGE
+                            from datetime import date
                             first_appointment_date = None
                             if emp['first_appointment_date'] and emp['first_appointment_date'] != 'None':
                                 try:
                                     first_appointment_date = pd.to_datetime(emp['first_appointment_date']).date()
                                 except:
-                                    first_appointment_date = datetime.now().date()
+                                    first_appointment_date = date.today()
                             else:
-                                first_appointment_date = datetime.now().date()
+                                first_appointment_date = date.today()
                             
-                            # FIXED: Added min_value=None, max_value=None to remove date limit
-                            first_appointment_date = st.date_input("First Date of Appointment", value=first_appointment_date, min_value=None, max_value=None, key="edit_appointment_date")
+                            # FIXED: Wide date range from 1900 to 2100
+                            first_appointment_date = st.date_input("First Date of Appointment", value=first_appointment_date, min_value=date(1900, 1, 1), max_value=date(2100, 12, 31), key="edit_appointment_date")
                             first_designation = st.text_input("First Designation", value=emp['first_designation'] if emp['first_designation'] else "", key="edit_first_designation")
                             first_job_group = st.text_input("First Appointment Job Group", value=emp['first_job_group'] if emp['first_job_group'] else "", key="edit_first_job_group")
                         
                         with col3:
-                            # Handle current designation date - NO DATE LIMIT
+                            # Handle current designation date - WIDE DATE RANGE
                             current_designation_date = None
                             if emp['current_designation_date'] and emp['current_designation_date'] != 'None':
                                 try:
                                     current_designation_date = pd.to_datetime(emp['current_designation_date']).date()
                                 except:
-                                    current_designation_date = datetime.now().date()
+                                    current_designation_date = date.today()
                             else:
-                                current_designation_date = datetime.now().date()
+                                current_designation_date = date.today()
                             
-                            # FIXED: Added min_value=None, max_value=None to remove date limit
-                            current_designation_date = st.date_input("Date of Current Designation", value=current_designation_date, min_value=None, max_value=None, key="edit_current_date")
+                            # FIXED: Wide date range from 1900 to 2100
+                            current_designation_date = st.date_input("Date of Current Designation", value=current_designation_date, min_value=date(1900, 1, 1), max_value=date(2100, 12, 31), key="edit_current_date")
                             current_designation = st.text_input("Current Designation", value=emp['current_designation'] if emp['current_designation'] else "", key="edit_current_designation")
                             current_job_group = st.text_input("Current Job Group", value=emp['current_job_group'] if emp['current_job_group'] else "", key="edit_current_job_group")
                         
